48b9bd4a24915eb5d5bdbfa7b2179002c473b8b4
Pacareu, Marc <marc.pacareu@sky.uk>
2024-05-09 08:14:37 +0100

    First commit

---
+++ src/main/java/com/bskyb/quarks/Game.java
@@ -0,0 +1,4 @@
+package com.bskyb.quarks;
+
+public class Game {
+}
---
+++ src/test/java/com/bskyb/quarks/GameTest.java
@@ -0,0 +1,12 @@
+package com.bskyb.quarks;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class GameTest {
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+}
